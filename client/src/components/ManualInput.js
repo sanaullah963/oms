8526e21592af2d7dd8a596e3c0442972e922728e
sanaullah963 <allhera963@gmail.com>
2025-11-29 21:13:11 +0600

    setLoading problem fix

--- client/src/components/ManualInput.js
+++ client/src/components/ManualInput.js
@@ -11,7 +11,7 @@ export default function ManualInput() {
 
   // State for Component
   const [inputValue, setInputValue] = useState("");
-  const [loading,   ] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState("");
 
   // Hydration Fix State
