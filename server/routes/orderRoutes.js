e322a6b760316ddcc4c8a43d03bb86c5bad4ef45
md sanaullah <116300167+sanaullah963@users.noreply.github.com>
2026-07-20 19:29:43 +0600

    Refactor ordercontroller to orderController

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const ordercontroller = require("../controllers/ordercontroller");
+const orderController = require("../controllers/orderController");
 const { bookSteadfast } = require("../controllers/steadfastController");
 const { bookSteadfastBulk } = require("../controllers/steadfastBulkController");
 
 router.get("/", ordercontroller.getOrders);
-router.post("/manual-single", ordercontroller.createManualOrder);
-router.delete("/delete/:id", ordercontroller.deleteOrder);
-router.put("/update-order/:id", ordercontroller.updateOrder);
-router.patch("/update-need-attention/:id", ordercontroller.updateNeedAttention);
-router.patch("/order-schedule/:orderId", ordercontroller.scheduleOrder);
+router.post("/manual-single", orderController.createManualOrder);
+router.delete("/delete/:id", orderController.deleteOrder);
+router.put("/update-order/:id", orderController.updateOrder);
+router.patch("/update-need-attention/:id", orderController.updateNeedAttention);
+router.patch("/order-schedule/:orderId", orderController.scheduleOrder);
 
 // courier booking
 router.post("/courier/steadfast/:orderId", bookSteadfast);
